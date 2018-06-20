2387bed30ab18387523096d71df9a75e386c5f6c
Aleph Yao <hyha600@hotmail.com>
2018-06-20 10:44:52 +0800

    Merging unnecesary files

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,12 @@
-printf "source '$HOME/dotfiles/zsh/zshrc_manager.sh'" > ~/.zshrc
-#printf "so $HOME/dotfiles/vim/vimrc.vim" > ~/.vimrc
-#printf "source-file $HOME/dotfiles/tmux/tmux.conf" > ~/.tmux.conf
+if command -v tmux>/dev/null; then
+  [ -z $TMUX  ] && exec tmux
+else
+  "tmux not installed!"
+fi
+
+echo "Updating Configuration..."
+(cd ~/dotfiles && git pull && git submodule update --init --recursive)
+
+printf "source $HOME/dotfiles/zsh/zshrc.sh" > ~/.zshrc
 exec zsh
 
